9fb204e922891650496a60f68e8f31acaaec5e43
Yacine Saada <yacinechaker@gmail.com>
2022-10-22 18:33:38 +0200

    build(feat_ModelAccount): création du modèle Account

--- src/main/java/tgpr/bank/model/Category.java
+++ src/main/java/tgpr/bank/model/Category.java
@@ -5,4 +5,37 @@ public class Category {
     private int id;
     private String name;
     private int account;
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", account=" + account +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAccount() {
+        return account;
+    }
+
+    public void setAccount(int account) {
+        this.account = account;
+    }
 }
